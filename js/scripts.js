a4c3bb9b86063c3af3b4cc8c23a6f0a7549e755d
JasonAtos <96166014+JasonAtos@users.noreply.github.com>
2021-12-19 18:15:08 -0600

    Workshop 2
    view languages simplified
    Added custom filter
    no data validation
    style editable on screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,37 @@
-$(document).ready(() => {
-    const tbody = document.getElementById("tbody");
-    const list = document.createDocumentFragment();
-    const url = 'https://restcountries.com/v3.1/all';
-
-    let options = {
-        numberPerPage:5, //Cantidad de datos por pagina
-        goBar:true, //Barra donde puedes digitar el numero de la pagina al que quiere ir
-        pageCounter:true, //Contador de paginas, en cual estas, de cuantas paginas
-    };
-
-    let filterOptions = {
-        el:'#searchBox' //Caja de texto para filtrar, puede ser una clase o un ID
-    };
+$(document).ready(() => {    
+    let tbody = document.getElementById("tbody");
+    let tr = [];
+    const url = 'https://restcountries.com/v3.1/all';        
+    let modal = document.getElementById("myModal");   
+    let noData = document.getElementById("noData");
+    noData.style.display = "none";
+    let editable = document.getElementById("editable");
+    editable.style.backgroundColor = "#ff0000";
+    let favcolor = document.getElementById("favcolor");
+    let span = document.getElementsByClassName("close")[0];    
+    let searchInput = document.querySelector("#customSearch");
+    let selectInput = document.querySelector("#customSelect");
+    let globalData = [];
+    let inputSearch = "";
+    let selectedColumn = "-1"; 
+    
 
     fetch(url)
     .then( response => response.json())
-    .then(data => { 
-        data.sort(
-            (a,b) => 
-            a.altSpellings[a.altSpellings.length-1].
-            localeCompare(b.altSpellings[b.altSpellings.length-1])
-        );
+    .then(data => {
+        getTableColumns();
+        if(!data) noData.style.display = "block";
+        data = sortAsc(data);
+        globalData = data;
+        displayData(data);
+ 
+    });
+    
+    const displayData = (data) => {            
+        const list = document.createDocumentFragment();
 
         data.map(country => {
-            let {altSpellings, capital, region, languages, population, flags:{png}} = country;
+            let {name:{common}, capital, region, population, flags:{png}} = country;
 
             let tr = document.createElement("tr");
             let tdNombre = document.createElement("td");
@@ -33,14 +41,10 @@ $(document).ready(() => {
             let tdPopulation = document.createElement("td");
             let tdImage = document.createElement("td");
             
-            let nombreOficial = altSpellings[altSpellings.length-1];
-            
-            tdNombre.innerHTML = `${nombreOficial}.`
+            tdNombre.innerHTML = `${common}.`
             tdCapital.innerHTML = `${capital}`;
             tdRegion.innerHTML = `${region}`;
-            tdLanguages.innerHTML = `${
-                languages && Object.keys(languages).map(key => languages[key])
-            }`;
+            tdLanguages.innerHTML = "<a href='#'>View languajes</a>";
             tdPopulation.innerHTML = `${population}`;
             tdImage.innerHTML = `<img src = ${png} width='100px' />`;
 
@@ -54,19 +58,22 @@ $(document).ready(() => {
             // tr.onclick = () => handleClick(nombreOficial);            
 
             list.appendChild(tr);            
-        });
-        tbody.appendChild(list);
+        });    
+                       
+        tbody.appendChild(list);                               
 
         document.querySelectorAll(".myTable").forEach(e => {                              
             e.addEventListener("click", (e) => {                
-                let oficialName = e.path[1].innerText.split(".")[0]; 
-                console.log(oficialName);
-                handleClick(oficialName);
+                let oficialName = e.path[1].innerText.split(".")[0];                 
+                if(oficialName === "View languajes")
+                    languajesModal(e, data);                                   
+                else              
+                    handleClick(oficialName);
             })
         }); 
-
-        paginate.init('.myTable',options,filterOptions);    
-    });   
+                
+        tr = tbody.getElementsByTagName("tr");        
+    };
 
     const handleClick = (nombreOficial) => {     
         fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${nombreOficial}`)
@@ -75,6 +82,86 @@ $(document).ready(() => {
             let message = data.extract_html ? data.extract_html : data.detail;            
             bootbox.alert(message)
         });
+    }
+
+    const languajesModal = (e, data) => {        
+        let countryName = e.path[2].innerText.split(".")[0];
+        let {languages} = data.filter(f=>f.name.common == countryName)[0];                                                        
+        const relleno = document.getElementById("modalRelleno");
+        relleno.innerHTML = "";                    
+        let ul = document.createElement("ul");
+        if(languages === undefined)
+            languages = {"No":"There's no languages"}
+        let languagesArray = [];     
+        Object.values(languages).map(value => languagesArray.push(value));
+        languagesArray.sort((a,b) => a.localeCompare(b));
+        languagesArray.forEach(language => {
+            let li = document.createElement("li");
+            li.innerText = language
+            ul.appendChild(li); 
+        });        
+        relleno.appendChild(ul);
+        modal.style.display = "block";
+    }
+
+    const sortAsc = (data) => {        
+        return data.sort(
+            (a,b) => a.name.common.localeCompare(b.name.common)
+        );
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+        modal.style.display = "none";
+        }
+    }
+
+    searchInput.addEventListener('keyup', (e) => {            
+        inputSearch = $("#customSearch").val().toLowerCase();        
+        if(inputSearch.length < 3) return  
+        let countContries = 0;        
+        for(let i = 0; i<tr.length; i++){
+            let td = selectedColumn == -1 ? 
+            tr[i] : tr[i].getElementsByTagName("td")[selectedColumn];
+                       
+            if(td){
+                let txtValue = td.textContent || td.innerText;
+                let countriesFounded = txtValue.toLowerCase().indexOf(inputSearch);
+                if(countriesFounded > -1){
+                    tr[i].style.display = "";
+                    countContries++;                    
+                }                                                    
+                else  {
+                   tr[i].style.display = "none";                   
+                   countContries--;
+                }                  
+                                                                  
+            }            
+        }   
+        
+        noData.style.display = countContries === -250
+        ? "block" : "none";
+    });
+
+    selectInput.addEventListener('change',(e) => {
+        selectedColumn = $( "#customSelect option:selected" ).val();        
+    });
+
+    favcolor.addEventListener('change', (e) => {
+        let selectecColor = $("#favcolor").val();
+        editable.style.backgroundColor = selectecColor;        
+    })
+    
+    const getTableColumns = () => {        
+        const {children} = document.getElementsByClassName("table-dark")[0];        
+        const {cells} = children[0];        
+        for(let i = 0; i<cells.length; i++){            
+            console.log(cells[i].innerText);         
+        }
     }
 
 });
